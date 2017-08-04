a54dd097ac89c3528f9e9872c67c0111f223753e
linkcoding <kenny.lin.91@gmail.com>
2017-08-04 15:40:54 -0400

    Done.

---
+++ app/models/listing.rb
@@ -0,0 +1,12 @@
+require 'pry'
+class Listing < ActiveRecord::Base
+  belongs_to :neighborhood
+  belongs_to :host, :class_name => "User"
+  has_many :reservations
+  has_many :reviews, through: :reservations
+  has_many :guests, :class_name => "User", through: :reservations
+
+end
+
+
+#Pry.start
